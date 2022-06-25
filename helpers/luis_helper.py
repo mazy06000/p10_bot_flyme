37a3742d27c79650ed500122bd5f3f79fcd602e7
Mazy <mazymohamed06@gmail.com>
2022-06-25 13:51:12 +0100

    entities in the response saved

--- helpers/luis_helper.py
+++ helpers/luis_helper.py
@@ -4,14 +4,12 @@ from enum import Enum
 from typing import Dict
 from botbuilder.ai.luis import LuisRecognizer
 from botbuilder.core import IntentScore, TopIntent, TurnContext
-
+from dateutil import parser
 from booking_details import BookingDetails
 
 
 class Intent(Enum):
-    BOOK_FLIGHT = "BookFlight"
-    CANCEL = "Cancel"
-    GET_WEATHER = "GetWeather"
+    BOOK_FLIGHT = "book"
     NONE_INTENT = "NoneIntent"
 
 
@@ -55,46 +53,56 @@ class LuisHelper:
                 result = BookingDetails()
 
                 # We need to get the result from the LUIS JSON which at every level returns an array.
-                to_entities = recognizer_result.entities.get("$instance", {}).get(
-                    "To", []
+                to_entities = recognizer_result.entities.get(
+                    "dst_city", []
                 )
                 if len(to_entities) > 0:
-                    if recognizer_result.entities.get("To", [{"$instance": {}}])[0][
-                        "$instance"
-                    ]:
-                        result.destination = to_entities[0]["text"].capitalize()
-                    else:
-                        result.unsupported_airports.append(
-                            to_entities[0]["text"].capitalize()
-                        )
+                    if recognizer_result.entities.get("dst_city")[0]:
+                        result.dst_city = to_entities[0].capitalize()
 
-                from_entities = recognizer_result.entities.get("$instance", {}).get(
-                    "From", []
+                from_entities = recognizer_result.entities.get(
+                    "or_city", []
                 )
                 if len(from_entities) > 0:
-                    if recognizer_result.entities.get("From", [{"$instance": {}}])[0][
-                        "$instance"
-                    ]:
-                        result.origin = from_entities[0]["text"].capitalize()
-                    else:
-                        result.unsupported_airports.append(
-                            from_entities[0]["text"].capitalize()
-                        )
+                    if recognizer_result.entities.get("or_city")[0]:
+                        result.or_city = from_entities[0].capitalize()
+
+
+                budget_entities = recognizer_result.entities.get(
+                    "budget", []
+                )
+                if len(budget_entities) > 0:
+                    if recognizer_result.entities.get("budget")[0]:
+                        result.budget = budget_entities[0]     
+
+                start_date_entities = recognizer_result.entities.get(
+                    "str_date", []
+                )
+                if len(start_date_entities) > 0:
+                    if recognizer_result.entities.get("str_date")[0]:
+                        result.str_date = parser.parse(start_date_entities[0]).strftime("%Y-%m-%d")
+                
+                end_date_entities = recognizer_result.entities.get(
+                    "end_date", []
+                )
+                if len(end_date_entities) > 0:
+                    if recognizer_result.entities.get("end_date")[0]:
+                        result.end_date = parser.parse(end_date_entities[0]).strftime("%Y-%m-%d")
 
                 # This value will be a TIMEX. And we are only interested in a Date so grab the first result and drop
                 # the Time part. TIMEX is a format that represents DateTime expressions that include some ambiguity.
                 # e.g. missing a Year.
-                date_entities = recognizer_result.entities.get("datetime", [])
-                if date_entities:
-                    timex = date_entities[0]["timex"]
+                # date_entities = recognizer_result.entities.get("datetime", [])
+                # if date_entities:
+                #     timex = date_entities[0]["timex"]
 
-                    if timex:
-                        datetime = timex[0].split("T")[0]
+                #     if timex:
+                #         datetime = timex[0].split("T")[0]
 
-                        result.travel_date = datetime
+                #         result.travel_date = datetime
 
-                else:
-                    result.travel_date = None
+                # else:
+                #     result.travel_date = None
 
         except Exception as exception:
             print(exception)
