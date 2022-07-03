272a8665446af1b355ff0d11a7e919ec78686802
Mazy <mazymohamed06@gmail.com>
2022-07-03 12:26:12 +0100

    env added

--- config.py
+++ config.py
@@ -4,6 +4,13 @@
 """Configuration for the bot."""
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
+LUIS_ID  = os.getenv("LUIS_ID")
+LUIS_KEY  = os.getenv("LUIS_KEY")
+LUIS_HOST  = os.getenv("LUIS_HOST")
+INSIGHTS_KEY  = os.getenv("INSIGHTS_KEY")
 
 
 class DefaultConfig:
@@ -12,10 +19,10 @@ class DefaultConfig:
     PORT = 3978#8000
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
-    LUIS_APP_ID = os.environ.get("LuisAppId", "3a14ce88-d39b-47c2-9661-12d37ec4d9e3")
-    LUIS_API_KEY = os.environ.get("LuisAPIKey", "af8aaf3777144547ae8083d82f1cfd58")
+    LUIS_APP_ID = os.environ.get("LuisAppId", LUIS_ID)
+    LUIS_API_KEY = os.environ.get("LuisAPIKey", LUIS_KEY)
     # LUIS endpoint host name, ie "westus.api.cognitive.microsoft.com"
-    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", "p10-luis-mazym.cognitiveservices.azure.com/")
+    LUIS_API_HOST_NAME = os.environ.get("LuisAPIHostName", LUIS_HOST)
     APPINSIGHTS_INSTRUMENTATION_KEY = os.environ.get(
-        "AppInsightsInstrumentationKey", "fa7baad5-5706-4dd5-bb13-252d8f8cceb9" #"c42ea6c6-8853-4278-b62b-d6a417481fd5"
+        "AppInsightsInstrumentationKey", INSIGHTS_KEY
     )
